a5f8f08f5bf9f46bec23eec68e4ee95152c378ec
Onewdivide <Onewdivide@hotmail.co.th>
2018-01-11 01:11:55 +0700

    add WheelsPicker
    add WheelsPicker that can choose destination in wheelsview like ios platform

--- app/src/main/java/com/example/onewdivide/myapplication1/ChooseDestination.java
+++ app/src/main/java/com/example/onewdivide/myapplication1/ChooseDestination.java
@@ -3,265 +3,112 @@ package com.example.onewdivide.myapplication1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class ChooseDestination extends AppCompatActivity {
+import com.aigestudio.wheelpicker.WheelPicker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class ChooseDestination extends AppCompatActivity implements WheelPicker.OnItemSelectedListener
+        , View.OnClickListener{
+    public List<String> Allplace = new ArrayList<String>();
+    public List<String> allPlace2Send = new ArrayList<String>();
+    public int thatPosition = 0;
+    public Button next;
+    private WheelPicker mainwheel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_destination);
 
-        Button Entrance1 = (Button)findViewById(R.id.Entrance1);
-        Entrance1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Entrance1");
-                startActivity(it);
-                finish();
-            }
-        });
+        Allplace.add("ทางเข้าหนึ่ง");
+        Allplace.add("บันไดหนึ่ง");
+        Allplace.add("ห้องน้ำชายหนึ่ง");
+        Allplace.add("ห้องน้ำหญิงหนึ่ง");
+        Allplace.add("ห้องสมุด");
+        Allplace.add("ห้องดีเอสเอส");
+        Allplace.add("ห้องเอที");
+        Allplace.add("ทางเข้าสอง");
+        Allplace.add("ประชาสัมพัน");
+        Allplace.add("ห้องหนึ่งศูนย์สอง");
+        Allplace.add("บันไดสอง");
+        Allplace.add("ลิป");
+        Allplace.add("ห้องหนึ่งศูนย์สี่");
+        Allplace.add("ห้องหนึ่งศูนย์ห้า");
+        Allplace.add("ห้องกิดจะกาน");
+        Allplace.add("ห้องหนึ่งศูนย์เจ็ด");
+        Allplace.add("ห้องหนึ่งศูนย์แปด");
+        Allplace.add("ห้องหนึ่งหนึ่งศูนย์");
+        Allplace.add("ห้องน้ำชายสอง");
+        Allplace.add("ห้องน้ำหญิงสอง");
+        Allplace.add("บันไดสาม");
+        Allplace.add("ร้านถ่ายเอกสาร");
+        Allplace.add("ห้องหนึ่งหนึ่งห้า");
+        Allplace.add("ห้องหนึ่งหนึ่งหก");
+        Allplace.add("ห้องหนึ่งหนึ่งแปด");
 
-        Button Ladder1 = (Button)findViewById(R.id.Ladder1);
-        Ladder1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Ladder1");
-                startActivity(it);
-                finish();
-            }
-        });
+        allPlace2Send.add("Entrance1");
+        allPlace2Send.add("Ladder1");
+        allPlace2Send.add("Toilet1Man");
+        allPlace2Send.add("Toilet1Woman");
+        allPlace2Send.add("Library");
+        allPlace2Send.add("DSSRoom");
+        allPlace2Send.add("ATRoom");
+        allPlace2Send.add("Entrance2");
+        allPlace2Send.add("PublicRelation");
+        allPlace2Send.add("Room102");
+        allPlace2Send.add("Ladder2");
+        allPlace2Send.add("Lift");
+        allPlace2Send.add("Room104");
+        allPlace2Send.add("Room105");
+        allPlace2Send.add("KKRoom");
+        allPlace2Send.add("Room107");
+        allPlace2Send.add("Room108");
+        allPlace2Send.add("Room110");
+        allPlace2Send.add("Toilet2Man");
+        allPlace2Send.add("Toilet2Woman");
+        allPlace2Send.add("Ladder3");
+        allPlace2Send.add("CopyStore");
+        allPlace2Send.add("Room115");
+        allPlace2Send.add("Room116");
+        allPlace2Send.add("Room118");
 
-        Button Toilet1Man = (Button)findViewById(R.id.Toilet1Man);
-        Toilet1Man.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Toilet1Man");
-                startActivity(it);
-                finish();
-            }
-        });
+        mainwheel = (WheelPicker) findViewById(R.id.mainwheel);
+        mainwheel.setData(Allplace);
+        mainwheel.setOnItemSelectedListener(this);
 
-        Button Toilet1Woman = (Button)findViewById(R.id.Toilet1Woman);
-        Toilet1Woman.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Toilet1Woman");
-                startActivity(it);
-                finish();
-            }
-        });
+        next = (Button) findViewById(R.id.thisbutton);
+        next.setOnClickListener(this);
 
-        Button Library = (Button)findViewById(R.id.Library);
-        Library.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Library");
-                startActivity(it);
-                finish();
-            }
-        });
+    }
 
-        Button DSSRoom = (Button)findViewById(R.id.DSSRoom);
-        DSSRoom.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","DSSRoom");
-                startActivity(it);
-                finish();
-            }
-        });
+    @Override
+    public void onItemSelected(WheelPicker picker, Object data, int position) {
 
-        Button ATRoom = (Button)findViewById(R.id.ATRoom);
-        ATRoom.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","ATRoom");
-                startActivity(it);
-                finish();
-            }
-        });
+        MyTTS.getInstance(ChooseDestination.this)
+                .setEngine("com.google.android.tts")
+                .setLocale(new Locale("th")).speak("");
 
-        Button Entrance2 = (Button)findViewById(R.id.Entrance2);
-        Entrance2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Entrance2");
-                startActivity(it);
-                finish();
-            }
-        });
+        switch (picker.getId()) {
+            case R.id.mainwheel:
+                thatPosition = position;
+                MyTTS.getInstance(ChooseDestination.this).speak(Allplace.get(position));
+                Log.e("mainwheels",Allplace.get(position));
+                Log.e("putExtra",allPlace2Send.get(position));
+                break;
+                }
+    }
 
-        Button PublicRelation = (Button)findViewById(R.id.PublicRelation);
-        PublicRelation.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","PublicRelation");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room102 = (Button)findViewById(R.id.Room102);
-        Room102.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room102");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Ladder2 = (Button)findViewById(R.id.Ladder2);
-        Ladder2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Ladder2");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Lift = (Button)findViewById(R.id.Lift);
-        Lift.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Lift");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room104 = (Button)findViewById(R.id.Room104);
-        Room104.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room104");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room105 = (Button)findViewById(R.id.Room105);
-        Room105.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room105");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button KKRoom = (Button)findViewById(R.id.KKRoom);
-        KKRoom.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","KKRoom");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room107 = (Button)findViewById(R.id.Room107);
-        Room107.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room107");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room108 = (Button)findViewById(R.id.Room108);
-        Room108.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room108");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room110 = (Button)findViewById(R.id.Room110);
-        Room110.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room110");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Toilet2Man = (Button)findViewById(R.id.Toilet2Man);
-        Toilet2Man.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Toilet2Man");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Toilet2Woman = (Button)findViewById(R.id.Toilet2Woman);
-        Toilet2Woman.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Toilet2Woman");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Ladder3 = (Button)findViewById(R.id.Ladder3);
-        Ladder3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Ladder3");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button CopyStore = (Button)findViewById(R.id.CopyStore);
-        CopyStore.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","CopyStore");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room115 = (Button)findViewById(R.id.Room115);
-        Room115.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room115");
-                startActivity(it);
-                finish();
-            }
-        });
-        Button Room116 = (Button)findViewById(R.id.Room116);
-        Room116.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room116");
-                startActivity(it);
-                finish();
-            }
-        });
-
-        Button Room118 = (Button)findViewById(R.id.Room118);
-        Room118.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
-                it.putExtra("Destination","Room118");
-                startActivity(it);
-                finish();
-            }
-        });
-
-
+    @Override
+    public void onClick(View v) {
+        Intent it = new Intent(getApplicationContext(),NavigationActivity.class);
+        it.putExtra("Destination",allPlace2Send.get(thatPosition));
+        startActivity(it);
+        finish();
     }
 }
